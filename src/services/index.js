e0167e5f248ee785f4d758e3c48c353e73e7ffd7
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-07-29 12:56:50 +0200

    add question service

--- src/services/index.js
+++ src/services/index.js
@@ -5,6 +5,7 @@ module.exports.authService = require('./auth.service');
 module.exports.emailService = require('./email.service');
 module.exports.tokenService = require('./token.service');
 module.exports.userService = require('./user.service');
+module.exports.viewService = require('./view.service');
 
 function ModelService(Model) {
     const create = async (dataBody, appFunct, errorMessage) => {
