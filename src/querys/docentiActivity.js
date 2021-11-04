d33ff04bb8c9756b081668744fb4e1dffd2c6fa5
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-11-04 08:31:35 +0100

    add querys

---
+++ src/querys/docentiActivity.js
@@ -0,0 +1,43 @@
+const queryDocentiActivity = idEnte => [
+    { "$match": { "idEnte": idEnte } },
+    { $lookup: { from: "corsis", localField: "idcorso", foreignField: "_id", as: "corso" } },
+    { "$unwind": "$corso" },
+    { "$unwind": "$docenti" },
+    {
+        "$project": {
+            "_id": 0,
+            "idDocente": "$docenti.id",
+            "idQuestion": "$_id",
+            "idcorso": "$idcorso",
+            "dataCorso": "$corso.dataInizio",
+            "codiceCorso": "$corso.codice",
+            "corso": "$corso.titolo",
+            "cognome": "$docenti.cognome",
+            "nome": "$docenti.nome",
+            "cf": "$docenti.cf",
+        }
+    },
+    {
+        $group: {
+            // Each `_id` must be unique, so if there are multiple
+            // documents with the same age, MongoDB will increment `count`.
+            _id: '$idDocente',
+            count: { $sum: 1 },
+            cognome: { $first: '$cognome'},
+            nome: { $first: '$nome'},
+            cf: { $first: '$cf'},
+            corsi: {
+                $push: {
+                    idcorso: '$idcorso',
+                    corso: '$corso',
+                    dataCorso: '$dataCorso',
+                    codiceCorso: '$codiceCorso',
+        }
+            }
+        }
+    }
+];
+
+
+
+module.exports.queryDocentiActivity = queryDocentiActivity;
---
+++ src/querys/index.js
@@ -0,0 +1,1 @@
+module.exports.queryDocentiActivity = require('./docentiActivity');
