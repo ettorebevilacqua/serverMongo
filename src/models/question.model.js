984086a46fa69c6d5969cc406d101b633af7a935
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-11-22 04:25:43 +0100

    TEMPORANEO ROTTO

--- src/models/question.model.js
+++ src/models/question.model.js
@@ -4,6 +4,9 @@ const bcrypt = require('bcryptjs');
 const { toJSON, paginate } = require('./plugins');
 const { roles } = require('../config/roles');
 const withRecordInfo = require('./withRecordInfo.schema');
+const { QuestionModuli } = require('./modulo.model');
+const ApiError = require('../utils/ApiError');
+const httpStatus = require('http-status');
 
 const questionSchema = mongoose.Schema(withRecordInfo({
     idEnte: {
@@ -54,7 +57,7 @@ const questionSchema = mongoose.Schema(withRecordInfo({
         token: String,
         sendCount: Number,
         sent: Boolean,
-        risposte:[{}]
+        risposte: [{}]
     }],
 }));
 
@@ -62,32 +65,123 @@ const questionSchema = mongoose.Schema(withRecordInfo({
 questionSchema.plugin(toJSON);
 questionSchema.plugin(paginate);
 
-questionSchema.statics.beforeServiceSave = async (Model, user, dataBody, id = 0) => {
-    const question = this;
+
+const empityRisposteDomandaModulo = idmodulo => (acc, domanda) => {
+    const iddomanda = domanda._id;
+    const risposte = domanda.risposte;
+    const domandaKey = {
+        idmodulo,
+        iddomanda,
+        risposte : risposte.map((risp, pos) => ({ pos, val: null })),
+    }
+   /* const list = risposte.map(risposta => ({
+        idmodulo,
+        iddomanda,
+        val: null,
+    })); */
+    return [...acc, domandaKey];
+    // console.log('aaaa cc', list);
+}
+
+const getDomandeModuli = async (idquestion) => {
+    let moduli = null;
+
+    try {
+        moduli = await QuestionModuli.findOne({ _id: idquestion }).populate('moduli');
+        if (!moduli) return false;
+    } catch (e) {
+        console.log('error xx', e);
+        return false;
+    }
+    if (!moduli.moduli) return [];
+    // console.log('xxxx getDomandeModuli', moduli.moduli);
+    return moduli.moduli.reduce((acc, modulo) => {
+        // console.log('aaaa cc', modulo);
+        const idModulo = modulo._id;
+        const domande = modulo.domande;
+        const empityRisposteDomanda = empityRisposteDomandaModulo(idModulo);
+        const list = domande.reduce(empityRisposteDomanda, acc);
+        return list;
+    }, []);
+}
+
+// questionSchema.statics.beforeServiceSave
+
+// addconst beforeServiceSave = async (Model, user, dataBody, id = 0) => {
+const beforeServiceSave = async (Model, user, dataBody, id = 0) => {
+    const question = Model;
     const { idquestion, idcorso, closeAt, _info, iduser } = { ...dataBody };
 
     if (question && question.closeAt && !!closeAt) {
         return [null, ' ' + ' question has close '];
     }
 
-    const found = await Model.findOne({ idquestion, idcorso });
+    // const found = await Model.constructor.findOne({ idquestion: '6191f87432b898ed1265f519', idcorso : '6191f87432b898ed1265f518'});
+    const found = await Model.constructor.findOne({ idquestion, idcorso });
     if (!!found && found._id != id) {
-        return [null, 'idcorso ' + idcorso + ' è già presente '];
+        return [null, 'il corso è già presente '];
     }
 
-    dataBody.idcorso = mongoose.Types.ObjectId(dataBody.idcorso);
-    dataBody.idquestion = mongoose.Types.ObjectId(dataBody.idquestion);
-    // console.log('xxxx to save dataBody', dataBody);
-
-    if (_info && _info.uc) { // && (!question || !iduser)) {
-        dataBody.iduser = question && question._info && question._info.uc ? question._info.uc : dataBody._info.uc;
+    if (!question.idquestion) {
+        return [dataBody, null];
     }
-    return [dataBody, null];
+
+    dataBody.idcorso = mongoose.Types.ObjectId(question.idcorso);
+    dataBody.idquestion = mongoose.Types.ObjectId(question.idquestion);
+
+    // console.log('xxxx to save idquestion', question.idquestion);
+    const domandeModuli = await getDomandeModuli(question.idquestion);
+
+    if (!domandeModuli) return [null, 'questionModuli not found id=' + idquestion];
+    const partecipanti = !question.partecipanti ? [] : question.partecipanti.map(el=>({ ...el }));
+    // console.log('xxxx to save partecipanti', domandeModuli);
+
+    const risposteToInsert = rispPart => domandeModuli.reduce((acc, risp) => {
+        const compareIds = el => el && el.idmodulo && el.iddomanda && el.idmodulo.toString() === risp.idmodulo.toString() && el.iddomanda.toString() === risp.iddomanda.toString();
+        const compiled = rispPart.find(compareIds);
+
+        if (compiled) return acc;
+        const idxAcc = acc.findIndex(compareIds);
+        console.log('xxxx risposteToInsert idxAcc', acc);
+        acc = idxAcc > -1 ? acc :  [...acc, risp];
+        // console.log('xxxx risposteToInsert', acc);
+        return acc;
+    }, []);
+
+    const partecipantiRisposte = partecipanti.map(partecipante => {
+        const part = { ...partecipante._doc };
+        const rispPart = [...part.risposte];
+
+        if (!rispPart) return { ...part, risposte };
+        console.log('xxxx partecipantiRisposte rispPart', rispPart);
+        const compiled = risposteToInsert(rispPart);
+        console.log('xxxx partecipantiRisposte', compiled);
+        const newRisposte = [...rispPart, ...compiled];
+        // console.log('xxxx partecipantiRisposte', [ ...rispPart, ...newRisposte ]);
+
+        return { ...part, risposte: newRisposte };
+    });
+    // console.log('xxxx to save partecipantiRisposte', partecipantiRisposte[0].risposte);
+    return [partecipantiRisposte, null];
 }
 
-questionSchema.pre('save', async function (next) {
+questionSchema.pre('save', async function (next, done) {
     const question = this;
+    // if (!!question._id) return  next();
+    const idquestion = question.idquestion;
+    // console.log('xxxx to save partecipanti', question);
+    const [partecipanti, err] = await beforeServiceSave(question, null, question, question._id);
+
+    if (err) {
+        question.invalidate("idcorso", err);
+        next(new ApiError(httpStatus.NOT_FOUND, err));
+        return false;
+    }
+    question.partecipanti=partecipanti;
+
     next();
+
+    // console.log('moduli ris', err);
 });
 
 
@@ -96,6 +190,11 @@ questionSchema.pre('save', async function (next) {
  */
 const Question = mongoose.model('Question', questionSchema);
 module.exports = Question;
+
+// getDomandeModuli('614d613684df521016d5f515').then(risp=>console.log('xxxx to save moduli',risp));
+
+ // getQuestionsModuli({_id: '618c2a96ca03116ab9d8f0c1'});
+// const { questionModuli } = require('../querys/');
 
 /*
 const { queryDocentiActivity } = require('../querys/');
--- src/querys/docentiActivity.js
+++ src/querys/docentiActivity.js
@@ -39,5 +39,43 @@ const queryDocentiActivity = idEnte => [
 ];
 
 
+const queryQuestionActivity = idEnte => [
+    { "$match": { "idEnte": idEnte + ''} },
+    { $lookup: { from: "questionmodulis", localField: "idquestion", foreignField: "_id", as: "question" } },
+    { $lookup: { from: "modulos", localField: "question.moduli", foreignField: "_id", as: "modulo" } },
+    { "$unwind": "$question" },
+    { "$unwind": "$modulo" },
+    { "$unwind": "$modulo.domande" },
+    { "$unwind": "$partecipanti" },
+    { "$unwind": "$partecipanti.risposte" },
 
-module.exports.queryDocentiActivity = queryDocentiActivity;
+    { "$match": { "idEnte": idEnte + ''} },
+
+    {
+        "$project": {
+            "_id": 0,
+            "idQuestion": "$_id",
+            "idcorso": "$idcorso",
+            "idmodulo": "$modulo._id",
+            "modulo": "$modulo.title",
+            "domanda": "$modulo.domande.domanda",
+            "iddomanda": "$modulo.domande._id",
+            "risposte": "$partecipanti.risposte",
+        }
+    },
+    /* {
+        $group: {
+            // Each `_id` must be unique, so if there are multiple
+            // documents with the same age, MongoDB will increment `count`.
+            _id: '$iddomanda',
+            domanda: {$first: '$domanda'},
+            count: { $sum: 1 },
+            risposte: {$push: '$risposte'},
+        }
+    }
+    */
+
+];
+
+
+module.exports={queryDocentiActivity, queryQuestionActivity};
