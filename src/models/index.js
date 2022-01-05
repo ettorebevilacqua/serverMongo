5c239793610063daecd1682a265a06a03ac4b1ff
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-01-05 10:37:41 +0100

    check if idente is present

--- src/models/index.js
+++ src/models/index.js
@@ -6,12 +6,15 @@ module.exports.Question = require('./question.model');
 module.exports.Corsi = require('./corsi.model');
 module.exports.Docenti = require('./docenti.model');
 module.exports.Ambiti = require('./ambiti.model');
+module.exports.Enti = require('./enti.model');
 module.exports.Lists = require('./lists.model');
 module.exports.Modulo = Modulo;
 module.exports.QuestionModuli = QuestionModuli;
 
 const addFieldUser = (fieldName, fieldUser) => (data, user) => {
-    return { ...data, [fieldName]: user[fieldUser] };
+     return !user || !user[fieldName] ?  { ...data }
+    : { ...data, [fieldName]: user[fieldUser] };
 };
 
-module.exports.addEnte = (fieldName = 'idEnte') => addFieldUser(fieldName, 'idEnte');
+module.exports.addEnte = (fieldName = 'idEnte') => addFieldUser(fieldName, 'idEnte'); 
+
